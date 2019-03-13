5acef8cfd641f6b16d87679c43826234487e713e
sreerajm <sreerajm@umich.edu>
2019-03-13 01:43:07 -0400

    update

--- rpn.py
+++ rpn.py
@@ -2,38 +2,35 @@
 
 import operator
 
+
 operators = {
-	'+':operator.add,
-	'-':operator.sub,
-	'*':operator.mul,
-	'/':operator.truediv,
-	'^':operator.pow,
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
-def calculate(arg):
-	stack = list()
-	for token in arg.split():
-		try:
-			value = int(token)
-			stack.append(value)
-		except ValueError:
-			function = operators[token]
-			
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1,arg2)
-			stack.append(result)
-			
-		print(stack)
-	if len(stack) != 1:
-		raise TypeError
-	return stack.pop()
-		
-	
-	
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-	while True:
-		print (calculate(input("rpn calc> ")))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
-if __name__ == "__main__":
-main()
+if __name__ == '__main__':
+    main()
--- test_rpn.py
+++ test_rpn.py
@@ -7,14 +7,11 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
     def test_subtract(self):
-        result = rpn.calculate("5 4 -")
-        self.assertEqual(1, result)
-    def test_multiply(self):
-        result = rpn.calculate("5 4 *")
-        self.assertEqual(20, result)
-    def test_divide(self):
-        result = rpn.calculate("8 4 /")
+        result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
-    def test_exponent(self):
-        result = rpn.calculate("2 3 ^")
-    self.assertEqual(8, result)
+    def test_multiply(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_divide(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
