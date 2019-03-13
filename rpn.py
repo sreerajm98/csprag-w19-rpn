88d68c2c587b6dddb2b9277f3e6ce0b2501666bd
sreerajm <sreerajm@umich.edu>
2019-03-13 01:37:49 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import operator
+
+operators = {
+	'+':operator.add,
+	'-':operator.sub,
+	'*':operator.mul,
+	'/':operator.truediv,
+	'^':operator.pow,
+}
+def calculate(arg):
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			function = operators[token]
+			
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1,arg2)
+			stack.append(result)
+			
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError
+	return stack.pop()
+		
+	
+	
+
+def main():
+	while True:
+		print (calculate(input("rpn calc> ")))
+
+if __name__ == "__main__":
+main()
